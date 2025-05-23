85924c692c1b90ad997dd7240da1b2fd0f2b7889
unknown <albreto.camachojr01@gmail.com>
2025-05-23 19:24:26 -0400

    Initial commit: Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg">
+      <h1 className="text-3xl font-bold underline">
+        Hello Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
+export default App;
